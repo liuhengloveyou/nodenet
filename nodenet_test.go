d9206154b017928e26266655dc56ff11f594009b
liuhengloveyou <liuhengloveyou@icloud.com>
2015-06-03 17:29:16 +0800

    添加注解

--- nodenet_test.go
+++ nodenet_test.go
@@ -12,18 +12,25 @@ func TestBuildFromConfig(t *testing.T) {
 }
 
 func TestNodenet(t *testing.T) {
+	// 从配置文件初始华
 	nodenet.BuildFromConfig("example/nodenet.conf.example")
 
+	// 找到名字为com1的组件, 把它的业务处理函数设置成自己实现的函数: work
 	nodenet.GetComponentByName("com1").SetHandler(work)
+
+	//启动这个组件
 	go nodenet.GetComponentByName("com1").Run()
 
+	// 新建一个消息
 	msg, _ := nodenet.NewMessage("demo", []string{"com1"}, "demo message")
 	fmt.Println(msg)
 
+	// 把消息发送给com1组件
 	err := nodenet.SendMsgToNext("com1", msg)
 	fmt.Println(err)
 }
 
+// 当组件收到一条消息的时候, 会调用work函数处理
 func work(msg interface{}) (result interface{}, err error) {
 	fmt.Println(">>>>>>", msg)
 
