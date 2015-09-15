f17fcacc4419495be0fba7ec3a4bdde796897425
liuhengloveyou <liuhengloveyou@icloud.com>
2015-09-15 16:06:57 +0800

    nodetype-id

--- woker.go
+++ woker.go
@@ -18,8 +18,9 @@ func GetWorkerByType(nodeType string) ComponentHandler {
 	return nil
 }
 
+// 节点名应该配置成: 节点业务类型-节点ID
 func GetWorkerByName(name string) ComponentHandler {
-	t := strings.Split(name, ".")
+	t := strings.Split(name, "-")
 	if len(t) < 1 {
 		return nil
 	}
